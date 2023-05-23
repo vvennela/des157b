43e9cd2769c64525228184afa15557e18a1070b5
vvennela <vishnupratikvennelakanti@gmail.com>
2023-05-22 17:29:10 -0700

    video files

---
+++ ministudios/library/script.js
@@ -0,0 +1,31 @@
+const { Chart } = require("chart.js");
+
+(function() {
+    'use strict';
+    const demo = document.getElementById('raceChart');
+
+    new Chart(demo, {
+        type: 'bar',
+        data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+
+
+
+
+
+
+})();
