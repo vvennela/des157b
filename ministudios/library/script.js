b09163cda6757c0660337894a25d39f8f79394c5
vvennela <vishnupratikvennelakanti@gmail.com>
2023-06-05 17:34:24 -0700

    hello

--- ministudios/library/script.js
+++ ministudios/library/script.js
@@ -5,23 +5,25 @@ const { Chart } = require("chart.js");
     const demo = document.getElementById('raceChart');
 
     new Chart(demo, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
+        type: 'pie',
+        data : {
+            datasets: [{
+                data: [8580, 7272, 6488, 4994, 1703, 618, 382, 105],
+                labels: [
+                    'Asian',
+                    'Hispanic',
+                    'White',
+                    'International',
+                    'Multi-ethnic',
+                    'Black',
+                    'Unknown',
+                    'Pacific Islander'
+            ]
+        }]
+    }, 
+
+
+    })
 
 
 
